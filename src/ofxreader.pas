29700c5f3f791d2163b3dfef877f3dee33a87991
Higor <higorstrobel@gmail.com>
2019-05-23 17:24:50 -0400

    Implementação de novas oções  para Credito e Debito

--- src/ofxreader.pas
+++ src/ofxreader.pas
@@ -158,10 +158,10 @@ begin
 
           if FindString('<TRNTYPE>', sLine) then
           begin
-             if (InfLine(sLine) = '0') or (InfLine(sLine) = 'CREDIT') then
+             if (InfLine(sLine) = '0') or (InfLine(sLine) = 'CREDIT') OR (InfLine(sLine) = 'DEP') then
                 oItem.MovType := 'C'
              else
-             if (InfLine(sLine) = '1') or (InfLine(sLine) = 'DEBIT') then
+             if (InfLine(sLine) = '1') or (InfLine(sLine) = 'DEBIT') OR (InfLine(sLine) = 'XFER') then
                 oItem.MovType := 'D'
              else oItem.MovType := 'OTHER';
           end;
